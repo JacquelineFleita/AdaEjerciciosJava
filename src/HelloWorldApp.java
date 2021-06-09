f8b592a58412876b21f587881bf75813b767b21d
JacquelineFleita <fleitajacqueline@hotmail.com>
2021-06-08 21:32:57 -0300

    agregar

---
+++ src/HelloWorldApp.java
@@ -0,0 +1,16 @@
+
+
+/**
+ * The HelloWorldApp class implements an application that simply prints "Hello
+ * World!" to standard output.
+ */
+public class HelloWorldApp {
+	// void (vacio) no devuelve nada al metodo
+	// Firma del metodo
+	// visibilidad + tipo de dato de retorno + nombre + (parametros)
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!" + "bienvenidas"); // Display the string.
+		System.out.println("Me llamo Jacqueline y tengo 24 años");
+	}
+}
